db69df5a504d58a4982442322c255d37c45500fa
Daryna Yusikova <darynayusikova@gmail.com>
2026-03-29 13:45:09 +0100

    finished the task

---
+++ src/components/App/App.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import css from './App.module.css'
+import SearchBar from '../SearchBar/SearchBar'
+import toast, { Toaster } from 'react-hot-toast'
+import fetchMovies from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+
+function App() {
+  const [moviesList, setMoviesList] = useState<Movie[]>([]);
+  const [loader, setLoader] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<boolean>(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [modal, setModal] = useState<boolean>(false);
+  async function FormHandler(submitQuery: string) {
+    setMoviesList([]);
+    setErrorMessage(false);
+    if (submitQuery === '') { toast.error('Please enter your search query.'); return }
+    setLoader(true);
+    try {
+      const movies: Movie[] | null = await fetchMovies(submitQuery);
+      if (movies === null) { setMoviesList([]); toast.error('No movies found for your request.'); } else {
+        setMoviesList(movies);
+      }
+    }   catch (err) { setErrorMessage(true);}
+    finally {
+      setLoader(false);
+    }
+}
+
+  function movieGridHandler(e: React.MouseEvent<HTMLLIElement>) {
+    const selectedMovieId = e.currentTarget.dataset.id; 
+    const movie = moviesList.find(movie => (String(movie.id) === selectedMovieId));
+    if (!movie) { return; }
+    setSelectedMovie(movie);
+    setModal(true);
+
+  }
+  
+  function closeModalHandler() { setModal(false); setSelectedMovie(null); }
+  
+
+  return (
+    <>
+      
+      <Toaster />
+      <SearchBar onSubmit={FormHandler} />
+      {loader ? <Loader /> : null}
+      {errorMessage ? <ErrorMessage /> : null}
+      {moviesList.length > 0 ? <MovieGrid onSelect={movieGridHandler} movies={moviesList} /> : null}
+      {selectedMovie && modal ? <MovieModal movie={selectedMovie} onClose={closeModalHandler} /> : null}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,41 @@
+import css from './SearchBar.module.css'
+interface SearchBar {
+    onSubmit: (submitQuery:string) => void
+}
+
+
+
+
+export default function SearchBar({ onSubmit }: SearchBar) {
+    function handleSubmit(formData:FormData) {
+        const submitQuery = formData.get('query') as string
+        onSubmit(submitQuery.trim());
+    }
+
+    return <header className={css.header}>
+  <div className={css.container}>
+    <a
+      className={css.link}
+      href="https://www.themoviedb.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by TMDB
+    </a>
+    <form action={handleSubmit} className={css.form}>
+      <input
+        className={css.input}
+        type="text"
+        name="query"
+        autoComplete="off"
+        placeholder="Search movies..."
+        autoFocus
+      />
+      <button className={css.button} type="submit">
+        Search
+      </button>
+    </form>
+  </div>
+</header>
+
+}
